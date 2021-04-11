ba29382346256a236da957017ffead28d3cfaa83
JanderRaul <janderr4ul@gmail.com>
2021-04-11 09:22:10 -0300

    Criacao do drawer

--- iveg/lib/menu/menu.dart
+++ iveg/lib/menu/menu.dart
@@ -9,7 +9,8 @@ class TelaMenu extends StatefulWidget {
 }
 
 class _TelaMenuState extends State<TelaMenu> {
-  var formKey = GlobalKey<FormState>();
+  var formKey = GlobalKey<FormState>();  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,7 +18,7 @@ class _TelaMenuState extends State<TelaMenu> {
       home: DefaultTabController(
           length: 2,
           child: Scaffold(
-            appBar: AppBar(
+            appBar: AppBar(              
               bottom: TabBar(
                 tabs: [
                   Tab(icon: Icon(Icons.people)),
@@ -34,21 +35,54 @@ class _TelaMenuState extends State<TelaMenu> {
               backgroundColor: Theme.of(context).primaryColor,
               centerTitle: true,
             ),
+            drawer: Drawer(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  DrawerHeader(
+                    child: Center(child: Text('Menu', style: TextStyle(fontSize: 24, color: Colors.white),)),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,                                
+                    ),
+                  ),
+                  ListTile(
+                    title: Text('Funcionalidade 1'),
+                    onTap: () {
+                      
+                    },
+                  ),
+                  ListTile(
+                    title: Text('Funcionalidade 2'),
+                    onTap: () {
+                      
+                    },
+                  ),
+                  ListTile(
+                    title: Text('Funcionalidade 3'),
+                    onTap: () {
+                      
+                    },
+                  ),
+                ],
+              ),
+            ),
             backgroundColor: Theme.of(context).backgroundColor,
-            body: TabBarView(
-              children: [
+            body: TabBarView(children: [
               Column(
                 children: [
-                  Produto('assets/imagens/marmita.jpeg','Marmita', 'R 20,00'),
-                  Produto('assets/imagens/hamburguer.jpeg', 'Hamburguer', 'R 15,00'),
-                  Produto('assets/imagens/macarronada.jpeg', 'Macarronada', 'R 15,00'),
+                  Produto('assets/imagens/marmita.jpeg', 'Marmita', 'R 20,00'),
+                  Produto('assets/imagens/hamburguer.jpeg', 'Hamburguer',
+                      'R 15,00'),
+                  Produto('assets/imagens/macarronada.jpeg', 'Macarronada',
+                      'R 15,00'),
                 ],
               ),
               Column(
                 children: [
-                  Produto('assets/imagens/magnus.jpeg','Magnus', 'R 20,00'),
-                  Produto('assets/imagens/pedigree.jpeg','Pedigree', 'R 15,00'),
-                  Produto('assets/imagens/faro.jpeg','Faro', 'R 15,00'),
+                  Produto('assets/imagens/magnus.jpeg', 'Magnus', 'R 20,00'),
+                  Produto(
+                      'assets/imagens/pedigree.jpeg', 'Pedigree', 'R 15,00'),
+                  Produto('assets/imagens/faro.jpeg', 'Faro', 'R 15,00'),
                 ],
               ),
             ]),
