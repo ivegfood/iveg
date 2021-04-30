7bf57ba2ea3b6e8c0e767d04731cf4b7086eacd7
JanderRaul <janderr4ul@gmail.com>
2021-04-30 00:59:57 -0300

    Atualização da tela perfil

--- iveg/lib/Perfil/BodyPerfil.dart
+++ iveg/lib/Perfil/BodyPerfil.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:iveg/Perfil/FotoPerfil.dart';
+import 'package:iveg/main.dart';
 
 class BodyPerfil extends StatelessWidget {
   const BodyPerfil({
@@ -7,6 +9,112 @@ class BodyPerfil extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Column());
+    return Column(
+      children: [
+        FotoPerfil(),
+        SizedBox(height: 20),
+        BotoesPerfil(
+          texto: 'Minha conta',
+          icone: Icons.person_outline,
+          press: () {},
+        ),
+        BotoesPerfil(
+          texto: 'Configurações',
+          icone: Icons.settings_outlined,
+          press: () {},
+        ),
+        BotoesPerfil(
+          texto: 'Ajuda',
+          icone: Icons.help_outline,
+          press: () {},
+        ),
+        Spacer(),
+        BotaoSair(
+          texto: 'Sair',
+          icone: Icons.logout,
+          press: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => TelaInicial()));
+          },
+        ),
+        SizedBox(height: 20)
+      ],
+    );
+  }
+}
+
+class BotoesPerfil extends StatelessWidget {
+  const BotoesPerfil({
+    Key key,
+    @required this.texto,
+    @required this.press,
+    @required this.icone,
+  }) : super(key: key);
+
+  final String texto;
+  final Function press;
+  final IconData icone;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: FlatButton(
+          color: Colors.white,
+          padding: EdgeInsets.all(20),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          onPressed: press,
+          child: Row(children: [
+            Icon(
+              icone,
+              size: 25,
+              color: Colors.green,
+            ),
+            SizedBox(width: 15),
+            Text(texto),
+            Spacer(),
+            Icon(Icons.arrow_forward_rounded, color: Colors.grey[100])
+          ])),
+    );
+  }
+}
+
+class BotaoSair extends StatelessWidget {
+  const BotaoSair({
+    Key key,
+    @required this.texto,
+    @required this.press,
+    @required this.icone,
+  }) : super(key: key);
+
+  final String texto;
+  final Function press;
+  final IconData icone;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: FlatButton(
+          color: Colors.red,
+          padding: EdgeInsets.all(20),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          onPressed: press,
+          child: Row(children: [
+            Icon(
+              icone,
+              size: 25,
+              color: Colors.white,
+            ),
+            SizedBox(width: 15),
+            Text(texto,
+                style: TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.bold)),
+            Spacer(),
+            Icon(Icons.arrow_forward_rounded, color: Colors.grey[100])
+          ])),
+    );
   }
 }
---
+++ iveg/lib/Perfil/FotoPerfil.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class FotoPerfil extends StatelessWidget {
+  const FotoPerfil({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 110,
+      width: 110,
+      child: Stack(            
+        fit: StackFit.expand,
+        overflow: Overflow.visible,
+        children: [
+          CircleAvatar(
+            backgroundImage: AssetImage("assets/imagens/JanderRaul.jpeg"),
+          ),
+          Positioned(
+            bottom: 0,
+            right: -24,
+            child: SizedBox(
+              height: 55,
+              width: 55,
+              child: FlatButton(
+                padding: EdgeInsets.zero,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25), 
+                  side: BorderSide(color: Colors.green)),
+                color: Colors.grey[300],
+                  onPressed: () {},
+                  child: Container(
+                    child: Icon(Icons.photo_camera_outlined, color: Colors.green)
+                  )),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
