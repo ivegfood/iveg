3ba3265a2d043ab897a64cfd70521c04841effb6
marceloclarosouza <marcelo.claro.zouza@gmail.com>
2021-10-06 21:57:10 -0300

    banco e pagamento

--- iveg/lib/pagamento/adicionarcartao.dart
+++ iveg/lib/pagamento/adicionarcartao.dart
@@ -2,40 +2,130 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:iveg/TelaFinal/TelaFinal.dart';
 import 'package:iveg/pagamento/alertdialog4.dart';
+// import 'package:flutter_credit_card_brazilian/credit_card_form.dart';
+// import 'package:flutter_credit_card_brazilian/credit_card_model.dart';
+import 'package:flutter_credit_card_brazilian/flutter_credit_card.dart';
+
 
 class AdicionarCartao extends StatefulWidget {
   @override
-  _AdicionarCartaoState createState() => _AdicionarCartaoState();
+  State<StatefulWidget> createState() {
+    return _AdicionarCartaoState();
+  }
 }
 
 class _AdicionarCartaoState extends State<AdicionarCartao> {
+  String cardNumber = '';
+  String expiryDate = '';
+  String cardHolderName = '';
+  String cvvCode = '';
+  bool isCvvFocused = false;
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+      title: 'Flutter Credit Card',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        resizeToAvoidBottomInset: true,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CreditCardWidget(
+                cardName: (String value) {
+                  print(value);
+                },
+                cardNumber: cardNumber,
+                expiryDate: expiryDate,
+                cardHolderName: cardHolderName,
+                cvvCode: cvvCode,
+                showBackView: isCvvFocused,
+                obscureCardNumber: true,
+                obscureCardCvv: true,
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: <Widget>[
+                      CreditCardForm(
+                        formKey: formKey,
+                        obscureCvv: true,
+                        obscureNumber: true,
+                        cardNumber: cardNumber,
+                        cvvCode: cvvCode,
+                        cardHolderName: cardHolderName,
+                        expiryDate: expiryDate,
+                        themeColor: Colors.blue,
+                        cardNumberDecoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Número do cartão',
+                          hintText: 'XXXX XXXX XXXX XXXX',
+                        ),
+                        expiryDateDecoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Validade',
+                          hintText: 'XX/XX',
+                        ),
+                        cvvCodeDecoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'CVV',
+                          hintText: 'XXX',
+                        ),
+                        cardHolderDecoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Nome completo',
+                        ),
+                        onCreditCardModelChange: onCreditCardModelChange,
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          primary: const Color(0xff1b447b),
+                        ),
+                        child: Container(
+                          margin: const EdgeInsets.all(8),
+                          child: const Text(
+                            'Validate',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'halter',
+                              fontSize: 14,
+                              package: 'flutter_credit_card',
+                            ),
+                          ),
+                        ),
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {
+                            showAlertDialog4(context);
+                          } else {
+                            print('Cartão inválido');
+                          }
+                        },
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
-  // Bandeiras disponíveis
-  List<String> listBand = [
-    'visa',
-    'mastercard',
-    'amex',
-    'elo',
-    'dinersclub',
-    'discover',
-    'jcb',
-    'aura',
-    'hiper',
-    'hipercard',
-    'rupay'
-  ];
-
-  _onTap() async {
-    // print('Numero do cartão: $creditCardNumber'); // Numero do cartão digitado
-    // print('Nome no cartão: $creditCardName'); // Nome no cartão digitado
-    // print('Valido até: $creditCardExpData'); // Validade do cartão
-    // print('CVV: $creditCardCVV'); // Código de segurança
-    // print('Bandeira: $creditCardBand'); // Bandeira do cartão
-    // print('CPF: $creditCardCPF'); // CPF do Titular
-    showAlertDialog4(context);
+  void onCreditCardModelChange(CreditCardModel? creditCardModel) {
+    setState(() {
+      cardNumber = creditCardModel!.cardNumber;
+      expiryDate = creditCardModel.expiryDate;
+      cardHolderName = creditCardModel.cardHolderName;
+      cvvCode = creditCardModel.cvvCode;
+      isCvvFocused = creditCardModel.isCvvFocused;
+    });
   }
 }
